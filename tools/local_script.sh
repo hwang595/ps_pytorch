1a83c7d45970300471837493aa30454e879e640f
hwang595 <hongyiwang@cs.wisc.edu>
2017-10-03 17:32:42 -0500

    modify scripts

--- tools/local_script.sh
+++ tools/local_script.sh
@@ -6,4 +6,4 @@ echo "Public address of master node: ${PUB_IP_ADDR}"
 ssh -o "StrictHostKeyChecking no" ubuntu@${PUB_IP_ADDR}
 scp -i ${KEY_PEM_DIR} ${KEY_PEM_DIR} ubuntu@${PUB_IP_ADDR}:~/.ssh
 scp -i ${KEY_PEM_DIR} hosts hosts_address config ubuntu@${PUB_IP_ADDR}:~/
-ssh -i ${KEY_PEM_DIR} ubuntu@${PUB_IP_ADDR} 'sudo apt-get update; git clone https://github.com/hwang595/pytorch_distributed_nn.git'
+ssh -i ${KEY_PEM_DIR} ubuntu@${PUB_IP_ADDR} 'sudo apt-get update; git clone https://github.com/hwang595/pytorch_distributed_nn.git; cp pytorch_distributed_nn/tools/remote_script.sh ~/'
--- tools/remote_script.sh
+++ tools/remote_script.sh
@@ -13,6 +13,8 @@ ssh-keygen -t rsa -b 4096 -C "hongyiwang.hdu@gmail.com"
 for i in $(seq 2 $DEEPLEARNING_WORKERS_COUNT);
   do
   scp -i ${KEY_PEM_NAME} id_rsa.pub deeplearning-worker${i}:~/.ssh
+  scp -i ${KEY_PEM_NAME} -r ~/pytorch_distributed_nn/pytorch_code/mnist_data deeplearning-worker${i}:~/pytorch_distributed_nn/pytorch_code/
+  scp -i ${KEY_PEM_NAME} -r ~/pytorch_distributed_nn/pytorch_code/cifar10_data deeplearning-worker${i}:~/pytorch_distributed_nn/pytorch_code/
   ssh -i ${KEY_PEM_NAME} deeplearning-worker${i} 'git clone https://github.com/hwang595/pytorch_distributed_nn.git; python pytorch_distributed_nn/pytorch_code/data_prepare.py; cd ~/.ssh; cat id_rsa.pub >> authorized_keys'
   echo "Done writing public key to worker: deeplearning-worker${i}"
  done
