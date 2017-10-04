9c7b9a99c066e6eaaf54a7ec00d2e469a37c5fb6
hwang595 <hongyiwang@cs.wisc.edu>
2017-10-03 19:48:03 -0500

    change script

--- tools/remote_script.sh
+++ tools/remote_script.sh
@@ -1,6 +1,8 @@
 KEY_PEM_NAME=HongyiScript.pem
 export DEEPLEARNING_WORKERS_COUNT=`wc -l < hosts`
-python pytorch_distributed_nn/pytorch_code/data_prepare.py
+cd ~/pytorch_distributed_nn/pytorch_code/
+python data_prepare.py
+cd ~
 
 sudo bash -c "cat hosts >> /etc/hosts"
 cp config ~/.ssh/
@@ -15,6 +17,6 @@ for i in $(seq 2 $DEEPLEARNING_WORKERS_COUNT);
   scp -i ${KEY_PEM_NAME} id_rsa.pub deeplearning-worker${i}:~/.ssh
   scp -i ${KEY_PEM_NAME} -r ~/pytorch_distributed_nn/pytorch_code/mnist_data deeplearning-worker${i}:~/pytorch_distributed_nn/pytorch_code/
   scp -i ${KEY_PEM_NAME} -r ~/pytorch_distributed_nn/pytorch_code/cifar10_data deeplearning-worker${i}:~/pytorch_distributed_nn/pytorch_code/
-  ssh -i ${KEY_PEM_NAME} deeplearning-worker${i} 'git clone https://github.com/hwang595/pytorch_distributed_nn.git; python pytorch_distributed_nn/pytorch_code/data_prepare.py; cd ~/.ssh; cat id_rsa.pub >> authorized_keys'
+  ssh -i ${KEY_PEM_NAME} deeplearning-worker${i} 'git clone https://github.com/hwang595/pytorch_distributed_nn.git; cd ~/.ssh; cat id_rsa.pub >> authorized_keys'
   echo "Done writing public key to worker: deeplearning-worker${i}"
  done
