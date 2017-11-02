020dad86cb589acdd170c197c8f903de9dbbcec9
hwang595 <hongyiwang@cs.wisc.edu>
2017-11-02 16:20:30 -0500

    debugs

--- pytorch_code/distributed_worker.py
+++ pytorch_code/distributed_worker.py
@@ -179,16 +179,17 @@ class DistributedWorker(NN_Trainer):
             #req_send_check, killed=self.network.backward_signal_kill(logits_1.grad, communicator=self.comm, req_send_check=req_send_check, cur_step=self.cur_step)
             
             # Try Timeout killing strategy this time:
+            '''
             try:
                 req_send_check = self.network.backward_timeout_kill(logits_1.grad, communicator=self.comm, req_send_check=req_send_check, cur_step=self.cur_step)
                 for req in req_send_check:
                     req.wait()
             except StopIteration:
                 print("Worker: {} Timeout".format(self.rank))
-
+            '''
             
             # Normal backward
-            req_send_check, killed=self.network.backward(logits_1.grad, communicator=self.comm, req_send_check=req_send_check)
+            req_send_check=self.network.backward(logits_1.grad, communicator=self.comm, req_send_check=req_send_check)
             # test here, this should complete the backward communication process:
             req_send_check[-1].wait()
             '''
