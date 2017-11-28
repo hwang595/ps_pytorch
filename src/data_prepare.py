f906ada28fe62dbd588faddea5855f7c71b99b31
hwang595 <hongyiwang@cs.wisc.edu>
2017-11-28 16:08:54 -0600

    modify data prepare script

--- src/data_prepare.py
+++ src/data_prepare.py
@@ -22,20 +22,20 @@ if __name__ == "__main__":
 	           transform=transforms.Compose([
 	               transforms.ToTensor(),
 	               transforms.Normalize((0.1307,), (0.3081,))]))
-	train_loader_mnist = torch.utils.data.DataLoader(training_set, batch_size=args.batch_size, shuffle=True)
+	train_loader_mnist = torch.utils.data.DataLoader(training_set_mnist, batch_size=128, shuffle=True)
 	test_loader_mnist = torch.utils.data.DataLoader(
 	    datasets.MNIST('./mnist_data', train=False, transform=transforms.Compose([
 	               transforms.ToTensor(),
 	               transforms.Normalize((0.1307,), (0.3081,))
-	           ])), batch_size=args.test_batch_size, shuffle=True)
+	           ])), batch_size=100, shuffle=True)
 	trainset_cifar10 = datasets.CIFAR10(root='./cifar10_data', train=True,
 	                                        download=True, transform=transforms.Compose([
 	               transforms.ToTensor(),
 	               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 	           ]))
-	train_loader_cifar10 = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
+	train_loader_cifar10 = torch.utils.data.DataLoader(trainset_cifar10, batch_size=128,
 	                                          shuffle=True)
 	test_loader_cifar10 = torch.utils.data.DataLoader(
 	    datasets.CIFAR10('./cifar10_data', train=False, transform=transforms.Compose([
 	               transforms.ToTensor(),  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-	           ])), batch_size=args.test_batch_size, shuffle=True)
+	           ])), batch_size=100, shuffle=True)
