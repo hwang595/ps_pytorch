f4ddafc05335ede11b9cefdff5ef9df47c30c9ab
hwang595 <hongyiwang@cs.wisc.edu>
2017-11-28 13:20:37 -0600

    modify data prepare

--- src/data_prepare.py
+++ src/data_prepare.py
@@ -3,6 +3,7 @@ Since we need to initialize the dataset in parallel, pre-download data is necess
 This script will do the job for you
 """
 import torch
+from torchvision import datasets, transforms
 #for tmp solution
 from mnist import mnist
 from datasets import MNISTDataset
@@ -11,7 +12,30 @@ from datasets import Cifar10Dataset
 
 
 if __name__ == "__main__":
+	'''
     # download data to directory ./mnist_data
     mnist_data = mnist.read_data_sets(train_dir='./mnist_data', reshape=True)
     # download data to directory ./cifar10_data
     cifar10_data = cifar10.read_data_sets(padding_size=0, reshape=True)
+    '''
+	training_set_mnist = datasets.MNIST('./mnist_data', train=True, download=True,
+	           transform=transforms.Compose([
+	               transforms.ToTensor(),
+	               transforms.Normalize((0.1307,), (0.3081,))]))
+	train_loader_mnist = torch.utils.data.DataLoader(training_set, batch_size=args.batch_size, shuffle=True)
+	test_loader_mnist = torch.utils.data.DataLoader(
+	    datasets.MNIST('./mnist_data', train=False, transform=transforms.Compose([
+	               transforms.ToTensor(),
+	               transforms.Normalize((0.1307,), (0.3081,))
+	           ])), batch_size=args.test_batch_size, shuffle=True)
+	trainset_cifar10 = datasets.CIFAR10(root='./cifar10_data', train=True,
+	                                        download=True, transform=transforms.Compose([
+	               transforms.ToTensor(),
+	               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+	           ]))
+	train_loader_cifar10 = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
+	                                          shuffle=True)
+	test_loader_cifar10 = torch.utils.data.DataLoader(
+	    datasets.CIFAR10('./cifar10_data', train=False, transform=transforms.Compose([
+	               transforms.ToTensor(),  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+	           ])), batch_size=args.test_batch_size, shuffle=True)
