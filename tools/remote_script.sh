9bc86e24d9d81966598323a661760526c48c8798
hwang595 <hongyiwang@cs.wisc.edu>
2017-12-08 19:55:47 -0600

    modify scripts and change to ibcast

--- tools/remote_script.sh
+++ tools/remote_script.sh
@@ -1,6 +1,6 @@
 KEY_PEM_NAME=HongyiScript.pem
 export DEEPLEARNING_WORKERS_COUNT=`wc -l < hosts`
-cd ~/pytorch_distributed_nn/src/
+cd ~/ps_pytorch/src/
 python data_prepare.py
 cd ~
 
@@ -15,8 +15,8 @@ ssh-keygen -t rsa -b 4096 -C "hongyiwang.hdu@gmail.com"
 for i in $(seq 2 $DEEPLEARNING_WORKERS_COUNT);
   do
   scp -i ${KEY_PEM_NAME} id_rsa.pub deeplearning-worker${i}:~/.ssh
-  ssh -i ${KEY_PEM_NAME} deeplearning-worker${i} 'git clone https://github.com/hwang595/pytorch_distributed_nn.git; cd ~/.ssh; cat id_rsa.pub >> authorized_keys'
-  scp -i ${KEY_PEM_NAME} -r ~/pytorch_distributed_nn/src/mnist_data deeplearning-worker${i}:~/pytorch_distributed_nn/src/
-  scp -i ${KEY_PEM_NAME} -r ~/pytorch_distributed_nn/src/cifar10_data deeplearning-worker${i}:~/pytorch_distributed_nn/src/
+  ssh -i ${KEY_PEM_NAME} deeplearning-worker${i} 'git clone https://github.com/hwang595/ps_pytorch.git; cd ~/.ssh; cat id_rsa.pub >> authorized_keys'
+  scp -i ${KEY_PEM_NAME} -r ~/ps_pytorch/src/mnist_data deeplearning-worker${i}:~/ps_pytorch/src/
+  scp -i ${KEY_PEM_NAME} -r ~/ps_pytorch/src/cifar10_data deeplearning-worker${i}:~/ps_pytorch/src/
   echo "Done writing public key to worker: deeplearning-worker${i}"
  done
